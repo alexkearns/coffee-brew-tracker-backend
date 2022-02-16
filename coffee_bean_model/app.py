715080979dc714f9cf6cbf34bddc1fc41baafbb6
Alex Kearns <alex@alexkearns.co.uk>
2022-02-16 19:33:01 +0000

    Add handling for multiple /coffee-bean routes

--- coffee_bean_model/app.py
+++ coffee_bean_model/app.py
@@ -1,10 +1,33 @@
 import json
+from aws_lambda_powertools import Tracer, Logger
+from aws_lambda_powertools.event_handler import APIGatewayRestResolver
+
+tracer = Tracer()
+logger = Logger()
+app = APIGatewayRestResolver()
 
 
-def lambda_handler(event, context):
-    response = {}
-
+@app.get("/coffee-bean")
+@tracer.capture_method(capture_response=False)
+def list_coffee_beans():
     return {
-        "statusCode": 200,
-        "body": json.dumps(response),
+        "coffeeBeans": [
+            {"name": "Brazil", "roaster": "Horsham Coffee Roasters"}
+        ]
     }
+
+
+@app.get("/coffee-bean/<bean_id>")
+@tracer.capture_method(capture_response=False)
+def get_coffee_bean(bean_id):
+    return {
+        "id": bean_id,
+        "name": "Brazil",
+        "roaster": "Horsham Coffee Roasters"
+    }
+
+
+@tracer.capture_lambda_handler(capture_response=False)
+@logger.inject_lambda_context
+def lambda_handler(event, context):
+    return app.resolve(event, context)
