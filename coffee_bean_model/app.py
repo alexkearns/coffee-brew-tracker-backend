00b84bcd03c83f7a0a2741955a3bba5a2baae8f6
Alex Kearns <alex@alexkearns.co.uk>
2022-02-23 19:47:34 +0000

    Add functionality for updating and deleting coffee beans

--- coffee_bean_model/app.py
+++ coffee_bean_model/app.py
@@ -1,5 +1,7 @@
 import boto3
 import os
+from botocore.exceptions import ClientError
+from uuid import uuid4
 from aws_lambda_powertools import Tracer, Logger
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.event_handler.exceptions import (
@@ -16,7 +18,8 @@ table = dynamodb.Table(os.getenv("DYNAMODB_TABLE"))
 
 def transform_coffee_bean_for_response(item):
     return {
-        "name": item["PK"],
+        "id": item["PK"],
+        "name": item["Name"],
         "strength": item["Strength"],
         "roaster": item["Roaster"],
         "tastingNotes": item["TastingNotes"]
@@ -41,15 +44,72 @@ def list_coffee_beans():
     }
 
 
+@app.put("/coffee-beans/<bean_id>")
+@tracer.capture_method(capture_response=False)
+def update_coffee_bean(bean_id):
+    json_payload = app.current_event.json_body
+    if "name" not in json_payload:
+        raise BadRequestError("name not included in request")
+
+    table.update_item(
+        Key={
+            "PK": bean_id,
+            "SK": "BEAN#METADATA"
+        },
+        UpdateExpression="SET #n = :name, Strength = :strength, Roaster = :roaster, TastingNotes = :tn",
+        ExpressionAttributeNames={
+            "#n": "Name"
+        },
+        ExpressionAttributeValues={
+            ":name": json_payload["name"],
+            ":strength": json_payload.get("strength", None),
+            ":roaster": json_payload.get("roaster", None),
+            ":tn": json_payload.get("tastingNotes", None)
+        }
+    )
+
+    return {
+        "message": f"{bean_id} has been successfully updated."
+    }
+
+
+@app.delete("/coffee-beans/<bean_id>")
+@tracer.capture_method(capture_response=False)
+def delete_coffee_bean(bean_id):
+    try:
+        table.delete_item(
+            Key={
+                "PK": bean_id,
+                "SK": "BEAN#METADATA"
+            },
+            ConditionExpression="attribute_exists(#n)",
+            ExpressionAttributeNames={
+                "#n": "Name"
+            }
+        )
+    except ClientError as err:
+        if err.response["Error"]["Code"] == "ConditionalCheckFailedException":
+            raise NotFoundError(f"Bean ID {bean_id} was not found")
+
+    return {
+        "message": f"{bean_id} has been successfully deleted."
+    }
+
 
 @app.get("/coffee-beans/<bean_id>")
 @tracer.capture_method(capture_response=False)
 def get_coffee_bean(bean_id):
-    return {
-        "id": bean_id,
-        "name": "Brazil",
-        "roaster": "Horsham Coffee Roasters"
-    }
+    response = table.get_item(
+        Key={
+            "PK": bean_id,
+            "SK": "BEAN#METADATA"
+        }
+    )
+
+    if "Item" not in response:
+        raise NotFoundError(f"Bean ID {bean_id} was not found")
+
+    return transform_coffee_bean_for_response(response["Item"])
 
 
 @app.post("/coffee-beans")
@@ -61,8 +121,9 @@ def add_coffee_bean():
         raise BadRequestError("name not included in request")
 
     data_to_add = {
-        "PK": json_payload["name"],
+        "PK": str(uuid4()),
         "SK": "BEAN#METADATA",
+        "Name": json_payload["name"],
         "Strength": json_payload.get("strength", None),
         "Roaster": json_payload.get("roaster", None),
         "TastingNotes": json_payload.get("tastingNotes", None)
@@ -73,7 +134,7 @@ def add_coffee_bean():
     )
 
     return {
-        "Message": f"{json_payload['name']} is added."
+        "message": f"{json_payload['name']} is added."
     }
 
 
